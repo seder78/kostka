861a00d376f70dce25e67e5fff2bdb2bb709fd83
seder78 <42831864+seder78@users.noreply.github.com>
2021-01-02 19:07:01 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+let cislo = 0
+function _7 () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . . . . .
+        `)
+}
+function zatreseni () {
+    cislo = randint(1, 7)
+    if (cislo == 1) {
+        _1()
+    } else if (cislo == 2) {
+        _2()
+    } else if (cislo == 3) {
+        _3()
+    } else if (cislo == 4) {
+        _4()
+    } else if (cislo == 5) {
+        _5()
+    } else if (cislo == 6) {
+        _6()
+    } else if (cislo == 7) {
+        _7()
+    } else {
+    	
+    }
+}
+function _2 () {
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . . . .
+        `)
+}
+function _6 () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+        `)
+}
+function _5 () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+}
+function _3 () {
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `)
+}
+input.onGesture(Gesture.Shake, function () {
+    zatreseni()
+})
+function _4 () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . . . .
+        `)
+}
+function _1 () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+}
+basic.forever(function () {
+	
+})
